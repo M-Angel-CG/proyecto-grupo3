b7b7a1e21d16e20ad986a7ee4b41387da8baefee
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-11-14 18:21:14 +0100

    structural adapter test

---
+++ src/test/java/com/example/proyectotesting/patterns/structural/adapter/CarTest.java
@@ -0,0 +1,30 @@
+package com.example.proyectotesting.patterns.structural.adapter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("CarTest")
+class CarTest {
+
+    Car car;
+
+    @BeforeEach
+    void setUp(){car = new Car();}
+
+    @Test
+    void setSpeed() {
+        car.setSpeed(99D);
+        assertEquals(99D,car.getSpeed());
+    }
+
+    @Test
+    @DisplayName("Añadir velocidad")
+    void SpeedUp() {
+        double initialspeed = car.getSpeed();
+        car.speedUp(1);
+        assertEquals(initialspeed+1,car.getSpeed());
+    }
+}
---
+++ src/test/java/com/example/proyectotesting/patterns/structural/adapter/TractorTest.java
@@ -0,0 +1,41 @@
+package com.example.proyectotesting.patterns.structural.adapter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TractorTest {
+
+    Tractor tractor;
+
+    @BeforeEach
+    void setUp(){tractor = new Tractor();}
+
+    @Test
+    void setSpeed() {
+        tractor.setSpeed(99D);
+        assertEquals(99D,tractor.getSpeed());
+    }
+
+    @Test
+    @DisplayName("Cambio a modo 1")
+    void changeMode1() {
+        tractor.changeMode(1);
+        assertEquals(5,tractor.getSpeed());
+    }
+    @Test
+    @DisplayName("Cambio a modo 2")
+    void changeMode2() {
+        tractor.changeMode(2);
+        assertEquals(15,tractor.getSpeed());
+    }
+    @Test
+    @DisplayName("Cambio fallido")
+    void changeMode0() {
+        tractor.setSpeed(0);
+        tractor.changeMode(99);
+        assertEquals(0,tractor.getSpeed());
+    }
+}

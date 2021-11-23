a7611bc8df2dc6fbd73121d72eb7c61e2f6099f8
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-11-23 21:10:23 +0100

    Test de Manufacturer edit

--- src/test/java/com/example/proyectotesting/selenium/BaseTest.java
+++ src/test/java/com/example/proyectotesting/selenium/BaseTest.java
@@ -19,8 +19,8 @@ public class BaseTest {
 
 
 
-        System.getenv().forEach((key, value) -> System.out.println(key + " " + value));
-        System.getProperties().forEach((key, value) -> System.out.println(key + " " + value));
+        //System.getenv().forEach((key, value) -> System.out.println(key + " " + value));
+        //System.getProperties().forEach((key, value) -> System.out.println(key + " " + value));
 
         if(System.getProperties().get("os.name").equals("Linux")){
             System.out.println("Configurando Navegador Chrome Headless para CI");
@@ -30,6 +30,7 @@ public class BaseTest {
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--headless");
             driver = new ChromeDriver(options);
+            js = (JavascriptExecutor) driver;
         }else{
             System.out.println("Configurando Navegador Chrome desde carpeta drivers para testing en desarrollo");
             //String dir = System.getProperty("user.dir"); // ruta del proyecto
@@ -37,6 +38,7 @@ public class BaseTest {
             //String url = dir + driverUrl;
             System.setProperty("webdriver.chrome.driver", driverUrl);
             driver = new ChromeDriver(); // Google Chrome
+            js = (JavascriptExecutor) driver;
         }
     }
 
